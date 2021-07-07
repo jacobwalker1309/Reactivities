d6d74d9a883fda047850939fdbbc90e1331126ff
Jacob <jacob.walker@accenture.com>
2021-07-07 10:52:38 +0100

    "End of section 5"

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,15 +34,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,10 +47,8 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,19 @@
+export interface Activity {
+    id: any;
+    title: any;
+    date: any;
+    description: any;
+    category: any;
+    city: any;
+    venue: any;
+    hostUsername: any | null;
+    isCancelled: any |null;
+    isGoing: any | null;
+    isHost: any | null;
+    
+  }
+
+  export class Activity {}
+  
+
+ 
